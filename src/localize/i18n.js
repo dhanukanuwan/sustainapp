7221b53e472ecbd8ef6c6e5db3d566cfa9df1bea
Dhanuka Gunarathna <nuwan.dhanuka@gmail.com>
2024-08-13 16:40:39 +0200

    setting translations cache expire to 24 hours

--- src/localize/i18n.js
+++ src/localize/i18n.js
@@ -56,7 +56,7 @@ i18n
         ],
         backendOptions: [
             {
-                expirationTime: 60 * 1000 // 1 day 1 * 24 * 60 * 60 * 1000
+                expirationTime: 1 * 24 * 60 * 60 * 1000 // 1 day
             },
             {
                 loadPath: `${API_BASE_URL}/sustainchange/v1/gettranslatedstrings/?lang={{lng}}`,
